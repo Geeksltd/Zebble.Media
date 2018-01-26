2c17cdfb0dfb1750f22578ef1002c62c51649224
Siavash <siavash.mohseni@geeks.ltd.uk>
2018-01-26 16:38:11 +0000

    Update Zebble package to 4.4.100

--- Android/Media.cs
+++ Android/Media.cs
@@ -7,6 +7,7 @@
     using System;
     using System.IO;
     using System.Threading.Tasks;
+    using Zebble.Services;
 
     partial class Media
     {
--- UWP/Media.cs
+++ UWP/Media.cs
@@ -1,5 +1,10 @@
 ﻿namespace Zebble.Device
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
     using Windows.Devices.Enumeration;
     using Windows.Media.Capture;
     using Windows.Storage;
